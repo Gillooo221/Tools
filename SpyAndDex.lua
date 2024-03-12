f48e2cfadcd8f3c4ed848288266b05df7f32d718
Ixroevich <155840318+Gillooo221@users.noreply.github.com>
2024-03-12 15:45:12 +0300

    Create SpyAndDex.lua

---
+++ SpyAndDex.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Gillooo221/ixHUB/main/SimpleSpy.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Gillooo221/ixHUB/main/DexV2.lua"))()
